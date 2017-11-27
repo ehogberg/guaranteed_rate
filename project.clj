fa78188890378e2629f0070753c5840821266621
Eric Hogberg <misc@erichogberg.com>
2017-11-26 18:12:12 -0600

    Whoops!  Update project def to include referenced libraries.

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.14.2"]]
+                 [clj-time "0.14.2"]
+                 [org.clojure/tools.cli "0.3.5"]]
   :main ^:skip-aot guaranteed-rate.core
   :target-path "target/%s"
+  :plugins [[lein-cloverage "1.0.10"]]
   :profiles {:uberjar {:aot :all}})
